44f3c3d20c3f438207aa845b841b74bf60efa40a
asianjohnboi <jbssantias@hotmail.com>
2018-07-28 12:38:02 +1000

    added new files

---
+++ POPme/background.js
@@ -0,0 +1,15 @@
+// When the chrome extension button is clicked, execute
+chrome.browserAction.onClicked.addListener(function () {
+	// Opens a new window of the given url
+    chrome.windows.create({ url: chrome.runtime.getURL("newtab.html") });
+});
+
+
+chrome.app.window.create('window.html', {
+  alwaysOnTop: true,
+}, function (appWindow) {
+  // Window created and will remain on top of others.
+
+  // Change the property programmatically via:
+  //appWindow.setAlwaysOnTop();
+});
