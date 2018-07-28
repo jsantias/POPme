0f6da77d7913d7a8a48f969564ec5c1f6e3bdec5
unknown <markusd9812@gmail.com>
2018-07-28 14:25:37 +1000

    merged all parts

--- POPme/background.js
+++ POPme/background.js
@@ -1,10 +1,19 @@
 // When the chrome extension button is clicked, execute
 chrome.browserAction.onClicked.addListener(function () {
-	// Opens a new window of the given url
-    chrome.windows.create({ url: chrome.runtime.getURL("newtab.html") });
+	function openWin() {
+		myWindow = window.open("popped-page.html", "", "width=880, height=520");
+	}
+	openWin();
+
 });
 
+/* Windows on top function 'currently does not work'
+chrome.windows.getCurrent(function(window){
+    chrome.windows.update(window.id,{top:0,left:0,width:screen.width,height:100})
+});
+*/
 
+/*
 chrome.app.window.create('window.html', {
   alwaysOnTop: true,
 }, function (appWindow) {
@@ -13,3 +22,5 @@ chrome.app.window.create('window.html', {
   // Change the property programmatically via:
   //appWindow.setAlwaysOnTop();
 });
+
+*/
--- POPme/eventPage.js
+++ POPme/eventPage.js
@@ -3,7 +3,7 @@ chrome.runtime.onInstalled.addListener(function() {
   var context = "all";
   var title = "POP ME BABY!"; //change me before submitting
   var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-                                         "id": "context" + context});  
+                                         "id": "context" + context});
 });
 
 // add click event
@@ -13,6 +13,6 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 // The onClicked callback function.
 function onClickHandler(info, tab) {
   var sText = info.selectionText;
-  var url = "https://www.google.com/search?q=" + encodeURIComponent(sText);  
+  var url = "https://www.google.com/search?q=" + encodeURIComponent(sText);
   window.open(url, '_blank'); //opens in new tab
 };
